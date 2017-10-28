a0dea0ff7b523b5b4f4343ff81103d33ece12e94
zhonglunsheng <zhonglunsheng@DESKTOP-6SV08CN>
2017-10-28 15:23:08 +0800

    按水果分类报表显示

--- src/cm/zls/chart/bar/BarChart.java
+++ src/cm/zls/chart/bar/BarChart.java
@@ -6,18 +6,17 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.servlet.ServletUtilities;
-import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.general.DatasetUtilities;
 
 public class BarChart {
 
 	public static String genBarChart(HttpSession session)throws Exception{
-		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-		dataset.addValue(510, "深圳", "苹果");
-		dataset.addValue(543, "深圳", "香蕉");
-		dataset.addValue(413, "深圳", "粒子");
-		dataset.addValue(123, "上海", "香蕉");
-		dataset.addValue(45, "北京", "粒子");
-		dataset.addValue(84, "成都", "橘子");
+		//按苹果分类显示
+		double [][]data = new double[][]{{1320},{720},{830},{400}};
+		String []rowKeys={"苹果","香蕉","橘子","梨子"};
+		String []columnKeys={"深圳"};
+		CategoryDataset dataset = DatasetUtilities.createCategoryDataset(rowKeys, columnKeys, data);
 		JFreeChart chart = ChartFactory.createBarChart3D("水果销售统计图", "水果", "销售", dataset, PlotOrientation.VERTICAL, true, true, true);
 		String fileName = ServletUtilities.saveChartAsJPEG(chart, 700, 500, null,session);
 		return fileName;
